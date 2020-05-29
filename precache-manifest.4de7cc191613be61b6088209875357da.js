168c8e4dd0b345c95b7601f2b311f03153df43c8
Marissol Karczeski <marfkar@gmail.com>
2020-05-29 14:11:10 -0700

    Updates

---
+++ precache-manifest.4de7cc191613be61b6088209875357da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e497d8b4121f0a4a55bd447a69cececb",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "47c9aaa4b9faa3f6c4ab",
+    "url": "/Employee-Directory-React/static/css/main.b6b8edb3.chunk.css"
+  },
+  {
+    "revision": "bb1019024bdb117736ea",
+    "url": "/Employee-Directory-React/static/js/2.330641bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Employee-Directory-React/static/js/2.330641bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c9aaa4b9faa3f6c4ab",
+    "url": "/Employee-Directory-React/static/js/main.4342f51f.chunk.js"
+  },
+  {
+    "revision": "c8115fa146be55d10016",
+    "url": "/Employee-Directory-React/static/js/runtime-main.082b6a87.js"
+  }
+]);
