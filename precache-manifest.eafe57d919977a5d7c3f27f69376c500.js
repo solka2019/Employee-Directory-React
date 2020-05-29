51fa301b11fe440df9025741cf8b990e283c9ea8
Marissol Karczeski <marfkar@gmail.com>
2020-05-28 22:44:32 -0700

    Updates

---
+++ precache-manifest.eafe57d919977a5d7c3f27f69376c500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cc92467a72450a7dc370c7d100cdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1742db2bffa1f581b7a5",
+    "url": "/static/css/main.b6b8edb3.chunk.css"
+  },
+  {
+    "revision": "bb1019024bdb117736ea",
+    "url": "/static/js/2.330641bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.330641bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1742db2bffa1f581b7a5",
+    "url": "/static/js/main.a01e4567.chunk.js"
+  },
+  {
+    "revision": "e7fd999373e62c96ddcd",
+    "url": "/static/js/runtime-main.af8ac8f5.js"
+  }
+]);
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ListResults from "./components/ListResults";
-
-function App() {
-  return <ListResults />;
-}
-
-export default App;
